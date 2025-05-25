eab95f2be0cb3c6ad8dc95f08ea669fc474ff3cd
1tzyb011 <itzahil46@gmail.com>
2025-05-25 14:01:24 +0530

    Update table generator.py

--- table generator.py
+++ table generator.py
@@ -2,6 +2,4 @@ num = int(input('enter the desired table number(Integers only): '))
 num2 = int(input('enter till which number you want your table(Integers only): '))
 for i in range(num2 + 1):
   print(num," X ", i, "=", num * i)
-
-
 print('\nTable done :)')
